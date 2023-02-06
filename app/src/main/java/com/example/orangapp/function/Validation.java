5debf4f78840e22dbb49dc359f160eba70d40db5
myharu01 <63537824+myharu01@users.noreply.github.com>
2023-02-06 22:44:17 +0900

    make validation function

--- app/src/main/java/com/example/orangapp/function/Validation.java
+++ app/src/main/java/com/example/orangapp/function/Validation.java
@@ -1,4 +1,19 @@
 package com.example.orangapp.function;
 
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import java.util.regex.Pattern;
+
 public class Validation {
+    public static boolean checkEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+    public static boolean checkPhoneNumber(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.PHONE.matcher(target).matches());
+    }
+    public static boolean checkPassword(CharSequence target) {
+        Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
+        return !TextUtils.isEmpty(target) && PASSWORD_PATTERN.matcher(target).matches();
+    }
 }
