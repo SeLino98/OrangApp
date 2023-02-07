2dbeb264b81e7b370d6b8eda819b8221e4f45ff2
lino <linos98@naver.com>
2023-02-07 13:32:10 +0900

    Authentication.class / email, phone sms

--- app/src/main/java/com/example/orangapp/function/Validation.java
+++ app/src/main/java/com/example/orangapp/function/Validation.java
@@ -6,12 +6,6 @@ import android.util.Patterns;
 import java.util.regex.Pattern;
 
 public class Validation {
-<<<<<<< HEAD
-
-
-
-
-=======
     public static boolean checkEmail(CharSequence target) {
         return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
@@ -22,5 +16,4 @@ public class Validation {
         Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
         return !TextUtils.isEmpty(target) && PASSWORD_PATTERN.matcher(target).matches();
     }
->>>>>>> 5debf4f78840e22dbb49dc359f160eba70d40db5
 }
